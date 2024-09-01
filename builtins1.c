150a9c755764f511e782687f25aa37745e7f99c1
abiodunalagbada <abiodunalagbada@gmail.com>
2024-09-01 11:35:59 +0200

    This has been edited

--- builtins1.c
+++ builtins1.c
@@ -22,17 +22,17 @@ int _myhistory(info_t *info)
  */
 int unset_alias(info_t *info, char *str)
 {
-	char *p, c;
+	char *a, b;
 	int ret;
 
-	p = _strchr(str, '=');
-	if (!p)
+	a = _strchr(str, '=');
+	if (!a)
 		return (1);
-	c = *p;
-	*p = 0;
+	b = *a;
+	*a = 0;
 	ret = delete_node_at_index(&(info->alias),
 		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-	*p = c;
+	*a = b;
 	return (ret);
 }
 
@@ -45,12 +45,12 @@ int unset_alias(info_t *info, char *str)
  */
 int set_alias(info_t *info, char *str)
 {
-	char *p;
+	char *a;
 
-	p = _strchr(str, '=');
-	if (!p)
+	a = _strchr(str, '=');
+	if (!a)
 		return (1);
-	if (!*++p)
+	if (!*++a)
 		return (unset_alias(info, str));
 
 	unset_alias(info, str);
@@ -65,15 +65,15 @@ int set_alias(info_t *info, char *str)
  */
 int print_alias(list_t *node)
 {
-	char *p = NULL, *a = NULL;
+	char *a = NULL, *d = NULL;
 
 	if (node)
 	{
-		p = _strchr(node->str, '=');
-		for (a = node->str; a <= p; a++)
-			_putchar(*a);
+		a = _strchr(node->str, '=');
+		for (d = node->str; d <= a; d++)
+			_putchar(*d);
 		_putchar('\'');
-		_puts(p + 1);
+		_puts(a + 1);
 		_puts("'\n");
 		return (0);
 	}
@@ -88,8 +88,8 @@ int print_alias(list_t *node)
  */
 int _myalias(info_t *info)
 {
-	int i = 0;
-	char *p = NULL;
+	int k = 0;
+	char *a = NULL;
 	list_t *node = NULL;
 
 	if (info->argc == 1)
@@ -102,13 +102,13 @@ int _myalias(info_t *info)
 		}
 		return (0);
 	}
-	for (i = 1; info->argv[i]; i++)
+	for (k = 1; info->argv[k]; k++)
 	{
-		p = _strchr(info->argv[i], '=');
-		if (p)
-			set_alias(info, info->argv[i]);
+		a = _strchr(info->argv[k], '=');
+		if (a)
+			set_alias(info, info->argv[k]);
 		else
-			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+			print_alias(node_starts_with(info->alias, info->argv[k], '='));
 	}
 
 	return (0);
